d580941397de6b8cbd5ce56dc87a45ff35022c36
nomysz <nomysz@yahoo.com>
2024-09-01 17:03:27 +0200

    Add radians normalization and comparing scalars

---
+++ math.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"math"
+)
+
+const (
+	PIDiv180 = math.Pi / 180.0
+	PI2      = math.Pi * 2
+)
+
+func IsEqualish(a, b float64) bool {
+	return math.Abs(b-a) < .0001
+}
+
+func NormalizeRadians(radians float64) float64 {
+	radians = math.Mod(radians, PI2)
+	if radians > math.Pi {
+		radians -= PI2
+	}
+	return radians
+}
